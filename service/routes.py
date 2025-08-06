fa421ea117de62f80599079feb4feeb5b8f41336
Li Wang <liwang955@outlook.com>
2025-08-06 06:44:17 +0000

    add pylint disable for duplicate code in routes

--- service/routes.py
+++ service/routes.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 ######################################################################
 
+# pylint: disable=duplicate-code, too-many-lines
 """
 Wishlist Service with Swagger
 
